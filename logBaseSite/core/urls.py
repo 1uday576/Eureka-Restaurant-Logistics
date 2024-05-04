3e4eace7ae3408506f4eb10847650b35a13223bc
1uday576 <126719344+1uday576@users.noreply.github.com>
2024-05-04 14:59:45 -0400

    integrate 1

--- logBaseSite/core/urls.py
+++ logBaseSite/core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homePage, name="homePage")
+    path("", views.homePage, name="landing"),
+    #path to the stock named 'stock'
+    path("stock", views.stock, name="stock")
 ]
